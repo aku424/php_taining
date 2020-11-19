6f610f4311c6da29f83f639e906cc8ef773462d6
aku424tt <61079024+aku424tt@users.noreply.github.com>
2020-11-19 17:25:34 +0900

    add file php & require_once

---
+++ php/products.php
@@ -0,0 +1,25 @@
+
+<?php 
+$products =[
+[
+    "name" => "アボカド",
+    "price" => 230,
+    "image" => "../img/image_1.jpg"
+],
+[
+    "name" => "オレンジ",
+    "price" => 330,
+    "image" => "../img/image_2.jpg"
+],
+[
+    "name" => " レモン",
+    "price" => 120,
+    "image" => "../img/image_3.jpg"
+],
+[
+    "name" => "パイナップル",
+    "price" => 500,
+    "image" => "../img/image_4.jpg"
+],
+];
+?>
