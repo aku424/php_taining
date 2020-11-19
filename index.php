3ec2ab84f68d20eac544c37766a628d23bb0e44d
aku424tt <61079024+aku424tt@users.noreply.github.com>
2020-11-19 17:16:30 +0900

    add foreach

--- index.php
+++ index.php
@@ -1,9 +1,26 @@
 <!-- php -->
 <?php 
-  $product =[
-    "name" => "アボカド",
-    "price" => 230,
-    "image" => "./img/image_1.jpg"
+  $products =[
+    [
+      "name" => "アボカド",
+      "price" => 230,
+      "image" => "./img/image_1.jpg"
+    ],
+    [
+      "name" => "オレンジ",
+      "price" => 330,
+      "image" => "./img/image_2.jpg"
+    ],
+    [
+      "name" => " レモン",
+      "price" => 120,
+      "image" => "./img/image_3.jpg"
+    ],
+    [
+      "name" => "パイナップル",
+      "price" => 500,
+      "image" => "./img/image_4.jpg"
+    ],
   ];
 ?>
 <!-- html -->
@@ -20,14 +37,16 @@
     <div class="app-container">
       <h1 class="title">DailyTrial Shopping</h1>
       <div class="cards-container">
-        <div class="card">
-          <img class="card-image" src="<?php echo $product["image"]?>" alt="">
-          <p class="card-title"><?php echo $product["name"]."円" ?></p>
-          <div class="flex justify-between">
-            <p class="card-price"><?php echo $product["price"] ?></p>
-            <input min="0" class="item-number" type="number" value="0">
-          </div>
-        </div>
+        <?php foreach($products as $product): ?>
+          <div class="card">
+              <img class="card-image" src="<?php echo $product["image"]?>" alt="">
+              <p class="card-title"><?php echo $product["name"]."円" ?></p>
+              <div class="flex justify-between">
+                <p class="card-price"><?php echo $product["price"] ?></p>
+                <input min="0" class="item-number" type="number" value="0">
+              </div>
+            </div>
+        <?php endforeach; ?>
       </div>
       <div class="btn-footer bg-white">
         <input class="cart-btn" type="submit" name="submit" value="カートに追加" />
